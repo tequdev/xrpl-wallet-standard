63baae5077029a7801caf52ad458c477fe4cd0b8
tequ <git@tequ.dev>
2024-08-03 15:32:48 +0900

    add react example

---
+++ packages/examples/react/src/App.tsx
@@ -0,0 +1,94 @@
+import './App.css'
+
+import { XRPL_MAINNET } from '@xrpl-wallet-standard/app'
+import {
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useSignAndSubmitTransaction,
+  useSignTransaction,
+  useWallet,
+  useWallets,
+} from '@xrpl-wallet-standard/react'
+
+function App() {
+  const wallets = useWallets()
+  const { wallet: selectedWallet } = useWallet()
+  const account = useAccount()
+  const { connect, status } = useConnect()
+  const disconnect = useDisconnect()
+  const signTransaction = useSignTransaction()
+  const signAndSubmitTransaction = useSignAndSubmitTransaction()
+
+  console.log(status)
+
+  const handleSignTransaction = async () => {
+    if (selectedWallet && account) {
+      const signedTransaction = await signTransaction(
+        {
+          TransactionType: 'Invoke',
+          Account: account.address,
+          NetworkID: 21338,
+        },
+        XRPL_MAINNET,
+      )
+      console.log(signedTransaction)
+    }
+  }
+
+  const handleSignAndSubmitTransaction = async () => {
+    const signedTransaction = await signAndSubmitTransaction(
+      {
+        TransactionType: 'AccountSet',
+        Account: account.address,
+      },
+      XRPL_MAINNET,
+    )
+    console.log(signedTransaction)
+  }
+
+  return (
+    <>
+      <h1>Wallet</h1>
+      <div className="card">
+        {wallets.map((wallet) => (
+          <div key={wallet.name}>
+            <button
+              type="button"
+              onClick={() => connect(wallet)}
+              className={`walletButton ${wallet.name === selectedWallet?.name ? 'walletButton-selected' : ''}`}
+            >
+              <img src={wallet.icon} alt="" height={24} />
+              <span>{wallet.name}</span>
+            </button>
+          </div>
+        ))}
+      </div>
+      <div className="card">
+        {selectedWallet && (
+          <div>
+            <h2>{selectedWallet.name}</h2>
+            {selectedWallet.accounts.map((account) => (
+              <div key={account.address}>{account.address}</div>
+            ))}
+          </div>
+        )}
+        {selectedWallet && account && (
+          <>
+            <button type="button" onClick={() => handleSignTransaction()}>
+              SignTransaction
+            </button>
+            <button type="button" onClick={() => handleSignAndSubmitTransaction()}>
+              SignAndSubmitTransaction
+            </button>
+            <button type="button" onClick={() => disconnect()}>
+              Disconnect
+            </button>
+          </>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ packages/examples/react/src/main.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { CrossmarkWallet } from '@xrpl-wallet-adapter/crossmark'
+import { WalletConnectWallet } from '@xrpl-wallet-adapter/walletconnect'
+import { XamanWallet } from '@xrpl-wallet-adapter/xaman'
+import { getWallets, registerWallet } from '@xrpl-wallet-standard/app'
+import { WalletProvider } from '@xrpl-wallet-standard/react'
+
+const additionalWallets = [
+  new XamanWallet('8f1280ed-374c-4b49-86ee-6dfbcdd4563f'),
+  new CrossmarkWallet(),
+  new WalletConnectWallet({
+    projectId: '85ad846d8aa771cd56c2bbbf30f7a183',
+    metadata: {
+      name: 'React App',
+      description: 'React App for WalletConnect',
+      url: 'https://walletconnect.com/',
+      icons: ['https://avatars.githubusercontent.com/u/37784886'],
+    },
+    networks: ['xrpl:mainnet'],
+  }),
+]
+
+const registeredWallets = getWallets().get()
+
+additionalWallets
+  .filter((wallet) => !registeredWallets.find((rw) => rw.name === wallet.name))
+  .forEach((wallet) => {
+    registerWallet(wallet)
+  })
+
+const rootElement = document.getElementById('root')
+if (!rootElement) throw new Error('Failed to find the root element')
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <WalletProvider>
+      <App />
+    </WalletProvider>
+  </React.StrictMode>,
+)
