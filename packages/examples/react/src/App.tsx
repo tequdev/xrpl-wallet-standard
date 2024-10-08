904b52c4e71db372782d3bfbe56bb3aad5ada5e6
tequ <git@tequ.dev>
2024-10-08 11:44:16 +0900

    update react examples

--- packages/examples/react/src/App.tsx
+++ packages/examples/react/src/App.tsx
@@ -1,79 +1,45 @@
 import './App.css'
 
-import { XRPL_TESTNET } from '@xrpl-wallet-standard/app'
 import {
   ConnectButton,
   useAccount,
   useConnectionStatus,
   useDisconnect,
   useSignAndSubmitTransaction,
-  useSignTransaction,
-  useWallet,
 } from '@xrpl-wallet-standard/react'
 
 function App() {
-  const { wallet: selectedWallet } = useWallet()
   const account = useAccount()
   const status = useConnectionStatus()
   const disconnect = useDisconnect()
-  const signTransaction = useSignTransaction()
   const signAndSubmitTransaction = useSignAndSubmitTransaction()
 
-  console.log(status)
-
-  const handleSignTransaction = async () => {
-    if (selectedWallet && account) {
-      const signedTransaction = await signTransaction(
-        {
-          TransactionType: 'Invoke',
-          Account: account.address,
-        },
-        XRPL_TESTNET,
-      )
-      console.log(signedTransaction)
-    }
-  }
-
   const handleSignAndSubmitTransaction = async () => {
-    const signedTransaction = await signAndSubmitTransaction(
+    const submittedTransaction = await signAndSubmitTransaction(
       {
         TransactionType: 'AccountSet',
         Account: account.address,
       },
-      XRPL_TESTNET,
+      'xrpl:1',
     )
-    console.log(signedTransaction)
+    console.log(submittedTransaction)
   }
 
   return (
     <>
-      <h1>Wallet</h1>
-      <div className="card">
-        <ConnectButton />
-      </div>
-      <div className="card">
-        {selectedWallet && (
-          <div>
-            <h2>{selectedWallet.name}</h2>
-            {selectedWallet.accounts.map((account) => (
-              <div key={account.address}>{account.address}</div>
-            ))}
-          </div>
-        )}
-        {selectedWallet && account && (
-          <>
-            <button type="button" onClick={() => handleSignTransaction()}>
-              SignTransaction
-            </button>
-            <button type="button" onClick={() => handleSignAndSubmitTransaction()}>
-              SignAndSubmitTransaction
-            </button>
-            <button type="button" onClick={() => disconnect()}>
-              Disconnect
-            </button>
-          </>
-        )}
-      </div>
+      <h1>Wallet Connect Example</h1>
+      {account && <div>{String(account.address)}</div>}
+      <ConnectButton />
+      {status === 'connected' && (
+        <div>
+          <button type="button" onClick={() => handleSignAndSubmitTransaction()}>
+            SignAndSubmitTransaction
+          </button>
+          <button type="button" onClick={() => disconnect()}>
+            Disconnect
+          </button>
+        </div>
+      )}
     </>
   )
 }
